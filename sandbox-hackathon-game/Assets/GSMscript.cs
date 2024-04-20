8161e0edd48f9d007cf48be352cf2cadd5671fb0
kmerk <mcmillenka@gmail.com>
2024-04-20 14:59:59 -0600

    updated game scene

--- sandbox-hackathon-game/Assets/GSMscript.cs
+++ sandbox-hackathon-game/Assets/GSMscript.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GSMscript : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float timer = 0;
+    public float stop = 10;
+    textFadeScript fs;
+    spriteSpawnerScript spawner;
     void Start()
     {
-        
+        fs = GetComponent<textFadeScript>();
+        spawner = GetComponent<spriteSpawnerScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timer > stop)
+        {
+            spawner.spawnIn = false;
+            fs.textComponent.text = "Round Over!";
+            fs.FadeInText();
+        }
+        timer += Time.deltaTime;
     }
+
+
 }
